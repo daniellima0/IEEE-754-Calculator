c027c7aaa15b435a64c4a28db3f74f27090c3b66
Daniel Lima <danielmlimag@gmail.com>
2021-11-26 12:07:10 -0300

    fix: trocar forma de conversão da parte decimal para binário

--- calculator.py
+++ calculator.py
@@ -1,16 +1,41 @@
-numero = input("Digite um número decimal: ") # supondo número 10.20
+def converterDecimalParaBinario(decimal, comprimento_decimal):
+    resultado = ""
 
-print("Número digitado: ", numero)
+    while comprimento_decimal:
+        decimal = str(decimal * 2)
+        
+        primeiro_digito = decimal[0:1]
 
-# Passo 1: Tranformar as partes à esquerda vírgula (parte inteira) e à direita da vírgula (parte decimal) em binário
-partes_do_numero = numero.split('.') # array [10, 20]
+        decimal = float(decimal)
+        
+        if primeiro_digito == "1":
+            decimal = decimal - 1
 
-[inteiro, decimal] = [partes_do_numero[0], partes_do_numero[1]]
+        resultado += primeiro_digito
 
-print("Parte inteira: ", inteiro)
-print("Parte decimal: ", decimal)
+        comprimento_decimal -= 1
+    return resultado
 
-[inteiro_binario, decimal_binario] = [bin(int(inteiro)), bin(int(decimal))]
 
-print("Parte inteira em binário: ", inteiro_binario)
-print("Parte decimal em binário: ", decimal_binario)
+numero = input("Digite um número decimal: ")    # supondo número 263.3
+tipo = 32   # 32 bits
+
+# Passo 1: Tranformar as partes inteira e decimal em binário
+inteiro = numero.split(".")[0]  # 263
+decimal = float(numero) - int(inteiro)  # 0.3
+
+print("Parte inteira:", inteiro) 
+print("Parte decimal:", decimal)
+
+inteiro_binario = bin(int(inteiro)).lstrip("0b")    # 100000111
+comprimento_sinal = 1
+comprimento_expoente = len(inteiro_binario) - comprimento_sinal # 8
+comprimento_decimal = 32 - (comprimento_sinal + comprimento_expoente)   # 23
+decimal_binario = converterDecimalParaBinario(decimal, comprimento_decimal) # 01001100110011001100110
+
+print("Parte inteira em binário:", inteiro_binario)
+print("Parte decimal em binário:", decimal_binario)
+
+# To Do List:
+# - Criar uma versão para 64 bits
+# - Perguntar a Aragão porque a parte decimal adquire uns números nas casas mais baixas
