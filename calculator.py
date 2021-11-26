c5fe837722321a3aae4ce87ad137ad5d23dba024
Daniel Lima <danielmlimag@gmail.com>
2021-11-26 13:35:45 -0300

    fix: limpar código e adicionar comentários

--- calculator.py
+++ calculator.py
@@ -1,62 +1,59 @@
-def converterDecimalParaBinario(decimal, comprimento_decimal):
+def converterDecimalParaBinario(decimal):
     resultado = ""
+    num_max = 32 - 2    # número máximo de bits que a parte decimal convertida em binário pode ter 
 
-    while comprimento_decimal:
+    while num_max:
         decimal = str(decimal * 2)
-
         primeiro_digito = decimal[0:1]
-
         decimal = float(decimal)
 
         if primeiro_digito == "1":
             decimal = decimal - 1
 
         resultado += primeiro_digito
+        num_max -= 1
 
-        comprimento_decimal -= 1
     return resultado
 
-
-numero = input("Digite um número decimal: ")  # supondo número 263.3
 tipo = 32  # 32 bits
+numero = input("Digite um número decimal: ")  # supondo número 263.3
 
 # Passo 1: Tranformar as partes inteira e decimal em binário
 inteiro = numero.split(".")[0]  # 263
 decimal = float(numero) - int(inteiro)  # 0.3
 
-print("Parte inteira:", inteiro)
+print("\nParte inteira:", inteiro)
 print("Parte decimal:", decimal)
 
 inteiro_binario = bin(int(inteiro)).lstrip("0b")  # 100000111
-comprimento_sinal = 1
-comprimento_expoente = len(inteiro_binario) - comprimento_sinal  # 8
-comprimento_decimal = 32 - (comprimento_sinal + comprimento_expoente)  # 23
 decimal_binario = converterDecimalParaBinario(
-    decimal, comprimento_decimal
-)  # 01001100110011001100110
+    decimal)  # 01001100110011001100110
 
-print("Parte inteira em binário:", inteiro_binario)
+print("\nParte inteira em binário:", inteiro_binario)
 print("Parte decimal em binário:", decimal_binario)
 
-# Passo 2:
+# Passo 2: Encontrar as devidas partes da representação (Sinal, Expoente e Mantissa)
 if float(numero) >= 0:
-    sinal = '0'
+    sinal = "0"
 else:
-    sinal = '1'
-
+    sinal = "1"
 print("\nSinal:", sinal)
 
+qtd_algarismo_significativo = 1  # número de algarismos significativos da notação científica
+comprimento_expoente = len(inteiro_binario) - qtd_algarismo_significativo  # 8
 bias = 127
-expoente = bin(127 + comprimento_expoente).lstrip("0b")
+expoente = bin(127 + comprimento_expoente).lstrip("0b") # 10000111
 print("Expoente:", expoente)
 
-comprimento_mantissa = 23 - len(inteiro_binario[1:])
-mantissa = inteiro_binario[1:] + decimal_binario[0:comprimento_mantissa]
-print("Mantissa:", mantissa)
+mantissa = inteiro_binario[1:] + decimal_binario    # 00000111010011001100110011001100110011
+mantissa_aproximada = mantissa[0:23]    # 00000111010011001100110
+print("Mantissa:", mantissa_aproximada)
 
-numero_convertido = sinal + expoente + mantissa
-print("Resultado:", numero_convertido)
+# Passo 3: Juntar todas as partes
+numero_convertido = sinal + expoente + mantissa_aproximada  # 01000011100000111010011001100110
+print("\nResultado:", numero_convertido, "\n")
 
 # To Do List:
+# - Descobrir por que a parte decimal adquire uns números nas casas mais à direita
+# - Implementar código para números negativos
 # - Criar uma versão para 64 bits
-# - Perguntar a Aragão porque a parte decimal adquire uns números nas casas mais baixas
